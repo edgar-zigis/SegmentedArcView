e1369553defcb788c12258a7853f9a7073faf7fc
Edgar Žigis <tranced.freak@gmail.com>
2021-01-28 09:40:16 +0200

    SAV-1: Implement custom sweep angles for individual segments

--- segmentedarcview/src/main/java/com/zigis/segmentedarcview/custom/ArcSegment.kt
+++ segmentedarcview/src/main/java/com/zigis/segmentedarcview/custom/ArcSegment.kt
@@ -3,5 +3,6 @@ package com.zigis.segmentedarcview.custom
 data class ArcSegment(
     val gradientStart: Int,
     val gradientEnd: Int,
-    val animate: Boolean = false
+    val animate: Boolean = false,
+    var sweepAngle: Float = 0f
 )
